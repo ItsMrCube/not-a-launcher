b580bae76688b2cc161e4dbbd8431ef6b1564029
ItsMrCube <matteo.ma.ad@gmail.com>
2022-11-13 09:08:21 +0100

    Made folder empty itself

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import { fetch } from "undici";
 import mods from "../mods.json" assert { type: "json" };
-import { writeFile } from "fs/promises";
+import fs from "fs/promises";
+import path from "path";
 
-for (const mod of mods) {
+for (const file of await fs.readdir(mods.folder)) {
+  await fs.unlink(path.join(mods.folder, file));
+}
+
+for (const mod of mods.mods) {
   const url = `https://api.modrinth.com/v2/project/${mod}/version`;
 
   const versions = (await fetch(url).then((res) => res.json())) as Version[];
@@ -15,7 +20,10 @@ for (const mod of mods) {
 
   const buf = await res.arrayBuffer();
 
-  await writeFile(`./mods/${version.filename}`, Buffer.from(buf));
+  await fs.writeFile(
+    path.join(mods.folder, version.filename),
+    Buffer.from(buf)
+  );
 
   console.log(`Downloaded ${version.filename}`);
 }
